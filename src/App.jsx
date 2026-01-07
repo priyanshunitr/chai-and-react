1fbad5c60be3d1310afce8f845b2f20385eb1e20
priyanshunitr <sahupriyanshu2006@gmail.com>
2026-01-08 01:04:08 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 import Chai from './chai'
 
 function App() {
+  const count = 0
   return (
     <>
       <Chai/>
+
+      <h2>Current count = {count}</h2>
+      <button>Add</button>
+      <div> </div>
+      <button>Remove</button>
     </>
   )
 }
---
+++ src/bonkers.jsx
@@ -0,0 +1,11 @@
+export default function Bonkers() {
+  return (
+<>
+
+
+<h1>sfeds</h1>
+
+
+
+</>
+)}
